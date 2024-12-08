7fa71a15b0a068e306c7e31b8e31154ee206f36b
nathan-n-poon <nathannpoon@gmail.com>
2024-12-07 23:15:20 -0800

    Initial commit

---
+++ borderify.js
@@ -0,0 +1,63 @@
+function error(errMsg) {
+    alert(errMsg + " :(")
+}
+
+function getValue(dict, key) {
+    let keyLocation = dict.search(key);
+    let lenKey = key.length;
+
+    // 1 = len("\"")
+    let start = dict.slice(keyLocation+lenKey).search("\"");
+    let end = dict.slice(keyLocation+lenKey+start+1).search("\"");
+    let val = dict.slice(keyLocation+lenKey+start+1, keyLocation+lenKey+start+end+1);
+    return val
+}
+
+function main() {
+
+    let theChildren = document.body.children;
+    var theChild;
+    for (var i = 0; i < theChildren.length; i++) {
+        if (theChildren[i].tagName == "SCRIPT" && theChildren[i].id == "") {
+            theChild = theChildren[i];
+            break;
+        }
+    }
+    let source = theChild.innerHTML;
+
+    let postedDate = getValue(source, "\"age\"");
+    console.log(postedDate);
+
+    // this data isnt hidden, but its displayed for user sanity check
+    let jobTitle = getValue(source, "\"jobTitle\"");
+    console.log(jobTitle);
+
+    if (postedDate == "") {
+        console.log("empty");
+        return
+    }
+
+    console.log("creating");
+
+    let infoBox = document.createElement('div');
+    infoBox.setAttribute("id", "infoBox");
+    infoBox.style.backgroundColor = "blue";
+    infoBox.style.height = "200px";
+    infoBox.style.width = "200px";
+    infoBox.style.position = "fixed";
+    infoBox.style.bottom = 0;
+    infoBox.style.right = 0;
+    infoBox.style.opacity = 0.8;
+
+    let text = document.createElement('p');
+    text.textContent = postedDate;
+    text.style.marginTop = Math.random()*200;
+    infoBox.appendChild(text);
+
+    document.body.appendChild(infoBox);
+    console.log("done creating");
+
+}
+
+main()
+
